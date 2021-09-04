3cb72b7822808f705db4cf8c049d3196d436013c
Dana Sugu <dana.sugu@gmail.com>
2021-09-04 20:19:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-
 // $words =  ["papaya", "mango", "apricot", "banana"];
+
 $words = file("words.txt");
 
 $word = $words[array_rand($words)];
-
+echo $word;
+echo "<br>";
 $wordLength = strlen($word);
-
+echo $wordLength;
+echo "<br>";
 for($i=1; $i<=$wordLength; $i++){
     echo "_ ";
 }
